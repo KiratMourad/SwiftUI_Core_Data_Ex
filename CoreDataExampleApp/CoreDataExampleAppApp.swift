2f4de56fb2de034dcaffc3c0a11f5f4275c8a961
Kirat <mourad.kirat.9@gmail.com>
2024-02-18 13:51:14 +0100

    Initial Commit

---
+++ CoreDataExampleApp/CoreDataExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataExampleAppApp.swift
+//  CoreDataExampleApp
+//
+//  Created by mac on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
